47142a9e3914b31483d12a3c46c913c20ff3a530
Viral Jani <viralj@maqsoftware.net>
2019-09-25 08:22:08 +0530

    Committed solution

---
+++ CityWeatherAnalysis/CityWeatherAnalysis/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace CityWeatherAnalysis
+{
+    class Program
+    {
+        static private string cityNamesData = @"E:\Prudential\InputFolder\City\Data.txt";
+        private static readonly string URL = "http://api.openweathermap.org/data/2.5/group";
+        private static readonly string appID = "aa69195559bd4f88d79f9aadeb77a8f6";
+        private static readonly string queryParameterID = "?id=";
+        private static readonly string queryParameterAppID = "&units=metric&appid=";
+        static private string outputFilePath = @"E:\Prudential\OutputFolder\";
+        static private string currentDate;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Read a text file line by line.
+                string[] cityLineItem = File.ReadAllLines(cityNamesData);
+                currentDate = DateTime.Now.ToString("M/d/yyyy");
+                currentDate = currentDate.Replace('/', '-');
+
+                // Get Count of Cities
+                int cityCount = cityLineItem.Length;
+
+                // Create an array to store city details
+                string[] cityDetails = new string[2];
+
+                foreach (string city in cityLineItem)
+                {
+                    cityDetails = city.Split('=');
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + queryParameterID + cityDetails[0] + queryParameterAppID + appID);
+                    try
+                    {
+                        WebResponse response = request.GetResponse();
+                        using (Stream responseStream = response.GetResponseStream())
+                        {
+                            StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+
+                            if (!Directory.Exists(outputFilePath + @"\" + cityDetails[1]))
+                            {
+                                Console.WriteLine(currentDate);
+                                Directory.CreateDirectory(outputFilePath + @"\" + cityDetails[1]);
+                            }
+
+                            File.WriteAllText(outputFilePath + @"\" + cityDetails[1] + @"\" + cityDetails[1] + "_" + currentDate + ".json", reader.ReadToEnd());
+
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        WebResponse errorResponse = ex.Response;
+                        using (Stream responseStream = errorResponse.GetResponseStream())
+                        {
+                            StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                            String errorText = reader.ReadToEnd();
+                            // log errorText
+                            Console.WriteLine(errorText);
+                        }
+                        throw;
+                    }
+
+                }
+
+                char c = Console.ReadKey().KeyChar;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                char c = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Prudential
+
+Prudential.zip file contains 3 folders.
+1.	InputFolder:
+a.	Create folders according to this path ‘E:\Prudential\InputFolder\City' and add file named as Data.txt.
+b.	This will be taken as input for the utility. 
+c.	If needed, change path to appropriate location of varibale named 'cityNamesData' for input 
+
+2.	OutputFolder:
+a.	This will be created automatically by utility. 
+b.	All the output files will be created under respective city names folder.
+
+3.	CityWeatherAnalysis:
+a.	This is the utility which will import city data, perform API call and store output.
+b.	To run utility, execute the EXE from folder 'CityWeatherAnalysis\CityWeatherAnalysis\bin\Debug'
+Test Cases
+1.	Check if the input file exists or not. In case if the file is not available, a console error will be printed.
+ 
+
+2.	In case of invalid city id or API call, appropriate message will be printed on Console.
+a.	In valid city ID – Error log
+ 
+
+b.	Improper AppID – Error log
+ 
+
+3.	In case of improper city ID from input file, utility will give appropriate message on console.
+ 
+
+Final Output:
+1.	Output will be stored in ‘E:\Prudential\OutputFolder’.
+2.	A folder for each city will be generated.
+ 
+
+3.	In the respective city name folder data for each day will be stored in json file.
+ 
